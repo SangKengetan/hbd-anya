54de014299cbd089fd212f0f86cc51bbe091ba65
SangKengetan <sang.kengetan@gmail.com>
2023-02-03 10:09:43 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'merah-aja' : '#780e18'
+      },
+      fontFamily: {
+        aksara: ['Noto Sans Devanagari'],
+        poppins:  ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
